a1d8fc3c58c9db9332ed8d5fc758816b5f9ed652
Hiromasa Fujimori <hiromasa.fujimori@microwavedigital.com>
2022-12-22 11:29:42 +0900

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,20 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+import MTMLHoverProvider from "./hoverProvider";
+
+const sel:vscode.DocumentSelector = { scheme: "file", language: "mtml" } ;
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext): void {
+  // console.log("hello mtml");
+
+  context.subscriptions.push(
+    // hover機能の提供開始
+    vscode.languages.registerHoverProvider(sel, new MTMLHoverProvider())
+  );
+}
+
+// this method is called when your extension is deactivated
+export function deactivate(): void {}
